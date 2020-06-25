00ad59b9f8ef2beed7043fde3445a1e0911afd10
raneamol <raneamol99@gmail.com>
2020-06-25 19:44:41 +0530

    Fixed cryptography key errors

--- app/main/accounts/routes.py
+++ app/main/accounts/routes.py
@@ -235,7 +235,8 @@ def send_email_after_transaction():
 
 	current_user = g.current_user
 	enc_email_pw = current_user["email_pw"]
-	cipher_key = environ.get("crypt_key")
+	crypt_key = environ.get("crypt_key")
+	cipher_key = crypt_key.encode('utf-8')
 	cipher_suite = Fernet(cipher_key)
 	enc_email_pw = enc_email_pw.encode('utf-8')
 	email_pw = cipher_suite.decrypt(enc_email_pw)
@@ -371,7 +372,8 @@ def order_stage_change():
 	current_user = g.current_user
 
 	enc_email_pw = current_user["email_pw"]
-	cipher_key = environ.get("crypt_key")
+	crypt_key = environ.get("crypt_key")
+	cipher_key = crypt_key.encode('utf-8')
 	cipher_suite = Fernet(cipher_key)
 	enc_email_pw = enc_email_pw.encode('utf-8')
 	email_pw = cipher_suite.decrypt(enc_email_pw)
@@ -593,7 +595,8 @@ def change_activity_type():
 
 	current_user = g.current_user
 	enc_email_pw = current_user["email_pw"]
-	cipher_key = environ.get("crypt_key")
+	crypt_key = environ.get("crypt_key")
+	cipher_key = crypt_key.encode('utf-8')
 	cipher_suite = Fernet(cipher_key)
 	enc_email_pw = enc_email_pw.encode('utf-8')
 	email_pw = cipher_suite.decrypt(enc_email_pw)
@@ -651,7 +654,8 @@ def get_order_from_email():
 
 	current_user = g.current_user
 	enc_email_pw = current_user["email_pw"]
-	cipher_key = environ.get("crypt_key")
+	crypt_key = environ.get("crypt_key")
+	cipher_key = crypt_key.encode('utf-8')
 	cipher_suite = Fernet(cipher_key)
 	enc_email_pw = enc_email_pw.encode('utf-8')
 	email_pw = cipher_suite.decrypt(enc_email_pw)
--- app/main/routes.py
+++ app/main/routes.py
@@ -4,8 +4,12 @@ from app.main import main
 
 from .helper import fetch_order
 
+from os import environ
+
+from cryptography.fernet import Fernet
 
 @main.route('/homepage')
 def homepage():
     abc = fetch_order()
+
     return str(abc)
