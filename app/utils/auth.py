1868bdc59d9b8dfb14c2b0afcbfce69c615783ef
raneamol <raneamol99@gmail.com>
2020-06-27 14:58:06 +0530

    Changed token parsing

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -10,18 +10,28 @@ from functools import wraps
 
 from os import environ
 
+from base64 import b64decode
+
 def token_required(f):
     @wraps(f)
     def decorated(*args, **kwargs):
         token = None
 
-        if 'access-token' in request.headers:
-            token = request.headers['access-token']
+        if 'Authorization' in request.headers:
+            auth_header = request.headers['Authorization']
+            auth = auth_header.split(maxsplit=2)
+            token = b64token = None
+            if len(auth) > 1 and auth[0].lower() == 'bearer':
+                b64token = auth[1]
+                try:
+                    token = b64decode(b64token)
+                except ValueError:
+                    pass
 
-        if not token:
+        if not b64token:
             return jsonify({'message' : 'Token is missing!'}), 401
         try: 
-            data = jwt.decode(token, environ.get('SECRET_KEY'))
+            data = jwt.decode(b64token, environ.get('SECRET_KEY'))
             current_user = mongo.Users.find_one({'username': data['username']})
             if current_user:
                 g.current_user = current_user
